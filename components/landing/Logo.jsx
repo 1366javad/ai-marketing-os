c2a20bfd092041890f84b544f750caf63f6bd8fa
javad13366 <javad.janjani1366@gmail.com>
2026-07-12 19:25:17 +0330

    Fix Cloudflare runtime using webpack build

--- components/landing/Logo.jsx
+++ components/landing/Logo.jsx
@@ -6,12 +6,7 @@ function Logo() {
   return (
     <Link href="/" className="flex items-center gap-3 group">
       <div className=" w-14 group-hover:scale-105 transition-transform">
-        <Image
-          src={logo}
-          alt="logo"
-          style={{ objectFit: "contain" }}
-          quality={100}
-        />
+        <img src="/logo.png" alt="logo" className="w-full h-auto" />
       </div>
     </Link>
   );
